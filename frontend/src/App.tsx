4ee519a30b1ad4ce067b37314f11bb0a1f2fb3f7
sulaksha-aththanayaka <sulakshaa04@gmail.com>
2026-02-13 17:02:47 +0530

    feat: initialize front end project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="w-full flex justify-center">
+      <h1 className="text-xl font-bold">Mini Task Tracker App</h1>
+    </div>
+  )
+}
+
+export default App
